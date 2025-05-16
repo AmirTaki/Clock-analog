94171b8e73f2d706ab9c715a833e0657d6c4498e
amir <amirtaki623@gmail.com>
2025-05-16 23:50:18 +0330

    js ; display time

---
+++ src/main.js
@@ -0,0 +1,58 @@
+let hour = document.getElementById("hour")
+let min = document.getElementById("min")
+let sec = document.getElementById("sec")
+
+const displayTime = () => {
+    let data = new Date();
+
+    // Getting hour, mins , secs from date
+}
+
+setInterval(displayTime, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+12 hour = 360 deg
+1 hour = 360 / 12 = 30
+h hours = 30h + m/2
+*/
+
+/*
+60min = 30 
+1min = 30/60 = 1/2
+m mins = (1/2)m
+*/
+
+/* 
+60min = 360deg
+1min = 360/60 = 6deg
+m min = 6m
+*/
+
+/*
+60sec = 360deg
+1 sec = 360/60 = 6deg
+s sec = 6s
+*/
